322b3569bad5b146fff1cbe897975d7649e418dd
Leora <cirem576@gmail.com>
2023-07-09 18:25:56 +0300

    fixed

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-// JSON dosyasını okuyoruz
-fetch("./imsak_vakitleri.json")
+fetch('./imsak_vakitleri.json')
   .then(response => response.json())
   .then(data => {
     // Arama işlemini yapan fonksiyon
     function searchCity() {
-      const city = document.getElementById("search").value.toLowerCase();
+      const searchElement = document.getElementById("search");
+      const city = searchElement.value.toLowerCase();
       const resultDiv = document.getElementById("result");
 
       // Eğer kullanıcı şehir ismi girmezse uyarı veriyoruz
@@ -16,30 +16,25 @@ fetch("./imsak_vakitleri.json")
       // Girilen şehir ismiyle eşleşen verileri buluyoruz
       const cityData = Object.keys(data).find(item => item.toLowerCase() === city);
 
-      if(!cityData){
-      // Eğer girilen şehir bulunamazsa uyarı veriyoruz
+      if (!cityData) {
+        // Eğer girilen şehir bulunamazsa uyarı veriyoruz
         resultDiv.innerHTML = `<p>${city} için imsak vakitleri bulunamadı.</p>`;
         return;
       }
 
       // Girilen şehir bulunursa imsak vakitlerini gösteriyoruz
-      const imsak = document.getElementById("imsak");
-      const ogle = document.getElementById("ogle");
-      const ikindi = document.getElementById("ikindi");
-      const aksam = document.getElementById("aksam");
-      const yatsi = document.getElementById("yatsi");
-
-      imsak.innerText = `İmsak: ${data[cityData].imsak}`;
-      ogle.innerText = `Öğle: ${data[cityData].ogle}`;
-      ikindi.innerText = `İkindi: ${data[cityData].ikindi}`;
-      aksam.innerText = `Akşam: ${data[cityData].aksam}`;
-      yatsi.innerText = `Yatsı: ${data[cityData].yatsi}`;
+      document.getElementById("imsak").innerText = `İmsak: ${data[cityData].imsak}`;
+      document.getElementById("ogle").innerText = `Öğle: ${data[cityData].ogle}`;
+      document.getElementById("ikindi").innerText = `İkindi: ${data[cityData].ikindi}`;
+      document.getElementById("aksam").innerText = `Akşam: ${data[cityData].aksam}`;
+      document.getElementById("yatsi").innerText = `Yatsı: ${data[cityData].yatsi}`;
     }
 
     // Arama butonuna tıklanınca veya "Enter" tuşuna basılınca searchCity() fonksiyonunu çağırıyoruz
     const searchButton = document.getElementById("search-btn");
     searchButton.addEventListener("click", searchCity);
-    document.querySelector('#search').addEventListener("keypress", event => {
+    const searchElement = document.getElementById("search");
+    searchElement.addEventListener("keypress", event => {
       if (event.key === "Enter") {
         searchCity();
       }
